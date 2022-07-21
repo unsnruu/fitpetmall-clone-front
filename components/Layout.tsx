9f2f36733ce73644b2af50e911ae26d9649bd967
unsnruu <unsnruu@gmail.com>
2022-07-21 12:33:42 +0900

    feat: implement basic structure of layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,26 @@
-function Layout() {
-  return <div>Layout</div>;
+import React, { PropsWithChildren } from "react";
+
+import Head from "next/head";
+import Header from "./Header";
+import Footer from "./Footer";
+
+interface LayoutProps extends PropsWithChildren {
+  title?: string;
 }
 
-export default Layout;
+export default function Layout({ title, children }: LayoutProps) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "핏펫몰",
+};
