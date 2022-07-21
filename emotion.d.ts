82ce353d3e2363187821545f6d5b90630c96e9ad
unsnruu <unsnruu@gmail.com>
2022-07-21 12:27:35 +0900

    feat: add global theme color

--- emotion.d.ts
+++ emotion.d.ts
@@ -4,6 +4,7 @@ declare module "@emotion/react" {
   export interface Theme {
     color: {
       gray: string;
+      blue: string;
     };
   }
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,31 @@
-import { Global, css } from "@emotion/react";
+import { Global, css, ThemeProvider, Theme } from "@emotion/react";
 import type { AppProps } from "next/app";
 
 const globalStyles = css`
   * {
     margin: 0;
+    padding: 0;
     box-sizing: border-box;
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui,
+      Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo",
+      "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol", sans-serif;
   }
 `;
+const theme: Theme = {
+  color: {
+    gray: `rgb(241, 243, 245);`,
+    blue: `rgb(20, 130, 255)`,
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Global styles={globalStyles} />
-      <Component {...pageProps} />;
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
