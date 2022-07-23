8b3a7ce669386fd1bce7573cfa9dd0a16709bc9e
unsnruu <unsnruu@gmail.com>
2022-07-23 16:26:51 +0900

    feat: add contents

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,41 @@
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div>
+        <span>이용약관</span>
+        <span>개인(위치)정보 처리방침</span>
+        <span>위치기반서비스 이용약관</span>
+      </div>
+      <div>
+        <span>고객센터</span>
+        <span>공지사항</span>
+      </div>
+      <div>
+        <span>평일 9:00~20:00 | 주말(토,일)</span>
+        <span>9:00~18:00 공휴일 휴무 | 점심시간 13:00~14:00</span>
+        <span>핏펫몰 입접 또는 제휴 문의 md@fitpet.co.kr </span>
+        <span>제휴 문의 contact@fitpet.co.kr </span>
+        <span>도매 문의 help@junglebook.co.kr</span>
+      </div>
+      <div>
+        <span>주식회사 판매 사업자 정보</span>
+        <p>
+          <span>상호: 주식회사 핏펫(Fitpet)</span>
+          <span>주소: 서울특별시 강남구 테헤란로 107길 6 2층 </span>
+          <span>대표: 고정욱</span>
+          <span>사업자번호: 543-87-00755 </span>
+          <span>사업자번호조회 통신판매업신고번호: 2018-서울강남-01774</span>
+          <span>개인정보관리자: 성주엽</span>
+          <span> 전화번호: 02-6339-1800 | 팩스번호: 02-6009-9068</span>
+          <span>이메일: help@fitpet.co.kr</span>{" "}
+          <span>구매안전서비스 : 신한은행 채무지급보증 안내</span>
+          <span>도매문의: contact@fitpet.co.kr</span>
+          <span>마케팅/제휴: marketing@fitpet.co.kr</span>
+        </p>
+      </div>
+      <div>Copyright Fitpet, Inc. All rights Reserved.</div>
+    </footer>
+  );
 }
 
 export default Footer;
