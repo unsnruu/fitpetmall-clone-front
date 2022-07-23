1117e3785d18cdd64f9f29d54e67c6a55e79e883
unsnruu <unsnruu@gmail.com>
2022-07-23 16:23:31 +0900

    feat: add scroll event to window object

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,20 +1,45 @@
-import React, { PropsWithChildren } from "react";
+import React, {
+  PropsWithChildren,
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+} from "react";
 
 import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
+import throttle from "utils/throttle";
 
 interface LayoutProps extends PropsWithChildren {
   title?: string;
 }
 
 export default function Layout({ title, children }: LayoutProps) {
+  const [position, setPosition] = useState(0);
+  const [expanded, setExpanded] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let moving = window.scrollY;
+      console.log("adsfasdf");
+      setExpanded(position > moving);
+      setPosition(moving);
+    };
+    const throttledHandleScroll = throttle(handleScroll);
+    window.addEventListener("scroll", throttledHandleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", throttledHandleScroll);
+    };
+  }, [position]);
+
   return (
     <div>
       <Head>
         <title>{title}</title>
       </Head>
-      <Header />
+      <Header expanded={expanded} />
       {children}
       <Footer />
     </div>
