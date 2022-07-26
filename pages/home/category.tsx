bcc08b5327669d06a506ee25ec3f504d104e8faf
unsnruu <unsnruu@gmail.com>
2022-07-26 21:10:15 +0900

    feat: implement category page

--- pages/home/category.tsx
+++ pages/home/category.tsx
@@ -1,5 +1,45 @@
+import styled from "@emotion/styled";
+
+import Link from "next/link";
+import Layout from "@components/Layout";
+import CategorySection from "@components/CategorySection";
+import BackIcon from "../../public/icons/ic_left.svg";
+import Head from "next/head";
+import Image from "next/image";
+
+const Header = styled.div`
+  height: 3rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  box-shadow: 0 1px 1px 0.1px ${({ theme }) => theme.color.gray};
+  margin: 0 1rem;
+`;
+const ImageWrapper = styled.a`
+  width: 50%;
+  cursor: pointer;
+`;
+const Title = styled.h3`
+  transform: translateX(-50%);
+`;
+
 function CategoryPage() {
-  return <div>Category Page</div>;
+  return (
+    <div>
+      <Head>
+        <title>핏펫몰</title>
+      </Head>
+      <Header>
+        <Link href="/">
+          <ImageWrapper>
+            <Image src={BackIcon} alt="go back" />
+          </ImageWrapper>
+        </Link>
+        <Title>카테고리</Title>
+      </Header>
+      <CategorySection />
+    </div>
+  );
 }
 
 export default CategoryPage;
